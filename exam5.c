e37415841860265b0ca191767ae975ec3ce98d69
sanjayparmar143 <118906048+sanjayparmar143@users.noreply.github.com>
2022-12-16 18:02:18 +0530

    Add files via upload

---
+++ exam5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main() {
+	
+	
+  int 20;
+  if 10;
+   {
+    Printf("Num is more than 10.")   
+    
+    
+    if num > 15 {
+      Printf("Num is also more than 15.")
+     }
+  } else {
+    Printf("Num is less than 10.")
+  }
+}
+}
